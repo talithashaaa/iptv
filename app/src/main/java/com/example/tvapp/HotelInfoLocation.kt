2374ef36792925ae45dc43ec2e336212272126cd
talithashaaa <talitafira23@gmail.com>
2024-02-02 17:55:50 +0700

    friday

---
+++ app/src/main/java/com/example/tvapp/HotelInfoLocation.kt
@@ -0,0 +1,11 @@
+package com.example.tvapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HotelInfoLocation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel_info_location)
+    }
+}
