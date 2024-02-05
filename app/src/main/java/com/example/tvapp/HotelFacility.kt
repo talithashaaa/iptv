45bf207225a4eb006a14a42f290c851268742dd5
talithashaaa <talitafira23@gmail.com>
2024-02-05 13:41:11 +0700

    monday

--- app/src/main/java/com/example/tvapp/HotelFacility.kt
+++ app/src/main/java/com/example/tvapp/HotelFacility.kt
@@ -1,11 +1,20 @@
 package com.example.tvapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tvapp.CardAdapter
+import com.example.tvapp.R
 
 class HotelFacility : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hotel_facility)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(this, 4)
+        recyclerView.adapter = CardAdapter()
     }
 }
--- app/src/main/java/com/example/tvapp/RecycleAdapterFacility.kt
+++ app/src/main/java/com/example/tvapp/RecycleAdapterFacility.kt
@@ -1,4 +1,48 @@
 package com.example.tvapp
 
-class RecycleAdapterFacility {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tvapp.R
+
+class CardAdapter : RecyclerView.Adapter<CardAdapter.ViewHolder>() {
+
+    private val images = intArrayOf(
+        R.drawable.restaurant,
+        R.drawable.bar,
+        R.drawable.gym,
+        R.drawable.rooftop,
+        R.drawable.working
+    )
+
+    private val text = arrayOf(
+        "Restaurant",
+        "Bar",
+        "Gym",
+        "Rooftop",
+        "Working Space"
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.facility, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemImage.setImageResource(images[position])
+        holder.itemText.text = text[position]
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var itemImage: ImageView = itemView.findViewById(R.id.cardImage)
+        var itemText: TextView = itemView.findViewById(R.id.cardText)
+    }
 }
