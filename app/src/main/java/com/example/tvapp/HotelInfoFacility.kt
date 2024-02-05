d331789351a4cc3a5756a4d31bc52a3b26ad0fa5
talithashaaa <talitafira23@gmail.com>
2024-02-05 13:40:09 +0700

    monday

---
+++ app/src/main/java/com/example/tvapp/HotelInfoFacility.kt
@@ -0,0 +1,11 @@
+package com.example.tvapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HotelInfoFacility : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel_info_facility)
+    }
+}
---
+++ app/src/main/java/com/example/tvapp/RecycleAdapterFacility.kt
@@ -0,0 +1,4 @@
+package com.example.tvapp
+
+class RecycleAdapterFacility {
+}
