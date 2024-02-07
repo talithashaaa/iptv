84cfb14116e3db69b9b6b4fa0d5511659c075e12
talithashaaa <talitafira23@gmail.com>
2024-02-07 18:49:36 +0700

    wednesday

---
+++ app/src/main/java/com/example/tvapp/OrderFoodPayment.kt
@@ -0,0 +1,11 @@
+package com.example.tvapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderFoodPayment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_food_payment)
+    }
+}
