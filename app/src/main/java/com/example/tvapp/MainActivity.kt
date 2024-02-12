ec05c17ea8d8969bfd7ff44d91ba84e4d05eadcb
talithashaaa <talitafira23@gmail.com>
2024-02-12 18:27:04 +0700

    monday 2

--- app/src/main/java/com/example/tvapp/MainActivity.kt
+++ app/src/main/java/com/example/tvapp/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.tvapp
 
 //import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import java.util.TimeZone
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTitle("Date & Time");
+
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Jakarta"))
+        val dateFormat = DateFormat.getDateInstance().format(calendar.time)
+
+        val dateTextView = findViewById<TextView>(R.id.text_date)
+        dateTextView.text = dateFormat
+
+        val dayFormat = SimpleDateFormat("EEEE", Locale.ENGLISH)
+        val dayTextView = findViewById<TextView>(R.id.text_day)
+        val dayOfWeek = dayFormat.format(calendar.time)
+        dayTextView.text = dayOfWeek
+
     }
 }
--- app/src/main/java/com/example/tvapp/SplashScreen.kt
+++ app/src/main/java/com/example/tvapp/SplashScreen.kt
@@ -1,11 +1,64 @@
 package com.example.tvapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Button
+import android.widget.TextView
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import java.util.TimeZone
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        setTitle("Date & Time");
+
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Jakarta"))
+        val dateFormat = DateFormat.getDateInstance().format(calendar.time)
+
+        val dateTextView = findViewById<TextView>(R.id.text_date)
+        dateTextView.text = dateFormat
+
+        val dayFormat = SimpleDateFormat("EEEE", Locale.ENGLISH)
+        val dayTextView = findViewById<TextView>(R.id.text_day)
+        val dayOfWeek = dayFormat.format(calendar.time)
+        dayTextView.text = dayOfWeek
+
+        val homeButton = findViewById<Button>(R.id.welcomeButton)
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        if (event.action == KeyEvent.ACTION_DOWN) {
+            when (event.keyCode) {
+                KeyEvent.KEYCODE_DPAD_CENTER, KeyEvent.KEYCODE_ENTER -> {
+                    navigateToMainActivity()
+                    return true
+                }
+                KeyEvent.KEYCODE_DPAD_LEFT, KeyEvent.KEYCODE_DPAD_RIGHT,
+                KeyEvent.KEYCODE_DPAD_UP, KeyEvent.KEYCODE_DPAD_DOWN -> {
+                    return true
+                }
+            }
+        }
+        return super.dispatchKeyEvent(event)
+    }
+
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
